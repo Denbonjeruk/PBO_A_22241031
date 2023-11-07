cfde63e59152ad113ec125d45c7fb12c57ddcceb
Denbonjeruk <129128663+Denbonjeruk@users.noreply.github.com>
2023-11-07 12:04:05 +0800

    Add files via upload

---
+++ 02-LKM/src/com/tutorial/Main.java
@@ -0,0 +1,36 @@
+package com.tutorial;
+class HitunganLingkaran{
+    int jarijari;
+    double phi;
+    double keliling,luas;
+
+    HitunganLingkaran( int jarijari,double phi){
+        this.jarijari=jarijari;
+        this.phi=phi;
+        this.keliling=2*phi*jarijari;
+        this.luas=phi*jarijari*jarijari;
+    }
+    void show(){
+
+    
+        System.out.println("======LINGKARAN======");
+        System.out.println("Jari-jari Lingkaran"+ this.jarijari+"cm");
+        System.out.println("keliling Lingkaran" +this.keliling+"cm");
+        System.out.println("Lingkaran" + this.luas+"cm2");
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        HitunganLingkaran h1= new HitunganLingkaran(27, 3.14);
+        h1.show();
+        HitunganLingkaran h2= new HitunganLingkaran(16, 3.14);
+        h2.show();
+        HitunganLingkaran h3= new HitunganLingkaran(19, 3.14);
+        h3.show();
+
+
+        
+}
+}
